01a6ebfa88c99e202f7fcd9cb3c6e027d8326f3d
Yanbin Deng <dyb0214@gmail.com>
2019-05-01 16:43:26 -0700

    added struct for data package"

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -31,12 +31,7 @@ void setup(){
   i2c_init();
   // (OPTIONAL) Check each address for a sensor
   address_polling_example();
-
-  /* 
-    Note that I placed the address change example in setup() for a good reason. Changing the sensor address causes an EEPROM write, there should only be ~1,000,000+
-      of these writes to the sensor microcontroller over its product lifetime. Changing the address is fine, but doing it every second for the next 4 years may cause reliability issues.
-  */
-  // (OPTIONAL) Run an address change example
+  
   //  default_address_change_example();  
 
   // Your code here
@@ -50,11 +45,13 @@ void loop()
   // (OPTIONAL) Read a sensor at the default address
 //  int value = read_the_sensor_example();   //Has been added to save sensor as a value 
   mux_read_sensor();
+  delay(50); // experiment with different value
+  for (int i = 0 ; i < SENSOR_COUNT; i++) {
+    trans_data(sensorReading[i], i);
+  }
 //  trans_data(value);   //Has been added to transmit value as a message to the reciever  
    
   // Your code here
-  
-
 }
 
 
@@ -62,7 +59,6 @@ void mux_read_sensor() {
     boolean error = 0;
     int range; 
     
-
     for (int i = 0; i < SENSOR_COUNT; i++) {
         error = start_sensor(addresses[i]);
         if (!error) {
@@ -71,13 +67,8 @@ void mux_read_sensor() {
              Serial.print(" : ");
              Serial.print(sensorReading[i]);
              Serial.print("\n");
-            // send address to LCD here
-            // L1: xxx
-            // L2: xxx
-            // R2: xxx
-            // R1: xxx
         }
-        delay(50); // experiment with different value
+        delay(100); // experiment with different value
     }
 }
 
@@ -85,11 +76,6 @@ void mux_read_sensor() {
 ///////////////////////////////////////////////////
 // Function: Start a range reading on the sensor //
 ///////////////////////////////////////////////////
-//Uses the I2C library to start a sensor at the given address
-//Collects and reports an error bit where: 1 = there was an error or 0 = there was no error.
-//INPUTS: byte bit8address = the address of the sensor that we want to command a range reading
-//OUPUTS: bit  errorlevel = reports if the function was successful in taking a range reading: 1 = the function
-//  had an error, 0 = the function was successful
 boolean start_sensor(byte bit8address){
   boolean errorlevel = 0;
   bit8address = bit8address & B11111110;               //Do a bitwise 'and' operation to force the last bit to be zero -- we are writing to the address.
@@ -98,7 +84,6 @@ boolean start_sensor(byte bit8address){
   i2c_stop();
   return errorlevel;
 }
-
 
 
 ///////////////////////////////////////////////////////////////////////
@@ -128,26 +113,42 @@ int read_sensor(byte bit8address){
 }
 
 
+//=================== Wireless section ===================
+// data pin to pin11
+//////////////////////////////////////////////
+//               Transmitter Setup          //
+//////////////////////////////////////////////
+//
+void trans_setup(){
+  //Serial.begin(9600);   //Debugger- no need for double Serial.begin(9600)
+  if(!driver.init())
+    Serial.println("init failed");
+}
+
+struct data_packages {
+  char measurement[3];
+  int sensor_number; 
+};
 
 
+//////////////////////////////////////////////
+//              Transmit data               //
+//////////////////////////////////////////////
+void trans_data(int msg, int sensor_number){
+  Serial.print("Transmit fcn test ");   //Test to see if the compiler is stepping through the fcn
+  char measurement [10]={0};    //char array
+  itoa(msg, measurement, 10);   //convert int to char array
 
-////////////////////////////////////////////////////////////
-//// Code Example: Read the sensor at the default address //
-////////////////////////////////////////////////////////////
-//int read_the_sensor_example(){      //Has been changed from void read_the_sensor_example() to int read_the_sensor_example()
-//  boolean error = 0;  //Create a bit to check for catch errors as needed.
-//  int range;
-//  
-//  //Take a range reading at the default address of 224
-//  error = start_sensor(address);    //Start the sensor and collect any error codes.
-//  if (!error){                  //If you had an error starting the sensor there is little point in reading it as you will get old data.
-//    delay(100);
-//    range = read_sensor(address);   //reading the sensor will return an integer value -- if this value is 0 there was an error
-//    Serial.print("R:");Serial.println(range);
-//  }
-//  return range; //Has been added to return the range fromt the sensor
-//}
-
+  struct data_packages data_package; 
+  itoa(msg, data_package.measurement, 10);
+  Serial.println(data_package.measurement);
+  
+//  Serial.println(sensor_no);    //Test to see if the itoa fcn is working
+  
+  const char *msg2 = measurement;   //Message to be sent  
+  driver.send((uint8_t*)msg2, strlen(msg2));    //Send Message
+  driver.waitPacketSent();
+}
 
 
 ////////////////////////////////////////////////////////////////
@@ -169,8 +170,6 @@ void address_polling_example(){
       Serial.println(i);
       if (range != 0){
         Serial.print("Device found at:");Serial.print(i);Serial.print(" Reported value of:");Serial.println(range);
-//        address=i;
-//        break;
       }  
     }
     else{
@@ -180,168 +179,3 @@ void address_polling_example(){
 
   Serial.println("Address polling complete.");
 }
-
-
-
-
-
-//=================== Wireless section ===================
-// data pin to pin11
-//////////////////////////////////////////////
-//               Transmitter Setup          //
-//////////////////////////////////////////////
-//
-void trans_setup(){
-  //Serial.begin(9600);   //Debugger- no need for double Serial.begin(9600)
-  if(!driver.init())
-    Serial.println("init failed");
-}
-
-//////////////////////////////////////////////
-//              Transmit data               //
-//////////////////////////////////////////////
-void trans_data(int msg){
-//  Serial.print("Transmit fcn test ");   //Test to see if the compiler is stepping through the fcn
-  char measurement [10]={0};    //char array
-  itoa(msg, measurement, 10);   //convert int to array
-//  Serial.println(measurement);    //Test to see if the itoa fcn is working
-  
-  const char *msg2 = measurement;   //Message to be sent  
-  driver.send((uint8_t*)msg2, strlen(msg2));    //Send Message
-  driver.waitPacketSent();
-  //delay(200);    //Delay between message (1 second)
-}
-
-
-///////////////////////////////////////////
-//// Function: Change the sensor address //
-///////////////////////////////////////////
-////Uses the I2C library to change the address of a sensor at a given address
-////Collects and reports an error bit where: 1 = there was an error or 0 = there was no error.
-////INPUTS: byte oldaddress = the current address of the sensor that we want to change
-////INPUTS: byte newddress  = the address that we want to change the sensor to
-////OUPUTS: bit  errorlevel = reports if the function was successful in changing the address: 1 = the function had an
-////      error, 0 = the function was successful
-//boolean change_address(byte oldaddress,byte newaddress){
-//  //note that the new address will only work as an even number (odd numbers will round down)
-//  boolean errorlevel = 0;
-//  oldaddress = oldaddress & B11111110;  //Do a bitwise 'and' operation to force the last bit to be zero -- we are writing to the address.
-//  errorlevel = !i2c_start(oldaddress) | errorlevel; //Start communication at the new address and track error codes
-//  errorlevel = !i2c_write(170) | errorlevel;        //Send the unlock code and track the error codes
-//  errorlevel = !i2c_write(165) | errorlevel;        //Send the unlock code and track the error codes
-//  errorlevel = !i2c_write(newaddress) | errorlevel; //Send the new address
-//  i2c_stop();
-//  return errorlevel;
-//}
-
-
-////////////////////////////////////////////////
-//// Code Example: Change the default address //
-////////////////////////////////////////////////
-//void default_address_change_example(){
-//  boolean error = 0;  //Create a bit to check for catch errors as needed.
-//  int range;
-//  
-//  Serial.println("Take a reading at the default address");
-//  
-//  //Take a range reading at the default address of 224
-//  error = start_sensor(224);    //Start the sensor and collect any error codes.
-//  if (!error){                  //If you had an error starting the sensor there is little point in reading it.
-//    delay(100);
-//    range = read_sensor(224);   //reading the sensor will return an integer value -- if this value is 0 there was an error
-//    Serial.print("R:");Serial.println(range);
-//  }
-//  
-//   Serial.println("Change the sensor at the default address to 222");
-//  //Change the address from 224 to 222
-//  error = 0;
-//  error = change_address(224,222);  //Change the address -- I don't do anything with the error handler at this point but you can if you want.
-//  delay(200);    //Wait 125ms for the sensor to save the new address and reset
-//  
-//   Serial.println("Take a reading at the new address");
-//  
-//  //Take a range reading at the new address of 222
-//  error = 0;
-//  error = start_sensor(222);     //Same as above but at the new address
-//  if (!error){
-//    delay(100);
-//    range = read_sensor(222);
-//    Serial.print("N:");Serial.println(range);
-//  }  
-//  
-//   Serial.println("Change the sensor back to the default address");  
-//  
-//  //Change the address from 222 to 224
-//  error = 0;
-//  error = change_address(222,224);
-//  delay(200);    //Wait 125ms for the sensor to save the new address and reset
-//}
-
-
-/*                   Arduino I2C for a MaxSonar                         */
-//////////////////////////////////////////////////////////////////////////
-//  Arduino I2C for a MaxSonar by Carl Myhre is licensed under a        //
-//  Creative Commons Attribution-ShareAlike 4.0 International License.  //
-//  Original Author:  Carl Myhre, 10-02-2014, Revision: 1.0             //
-//  Modifications by:                                                   //
-//                                                                      //
-//  Revision History: 1.0 -- 10-02-2014 -- Created initial code build   //
-//                                                                      //
-//  The original I2C libraries were created by Peter Fleury             //
-//    http://homepage.hispeed.ch/peterfleury/avr-software.html          //
-//                                                                      //
-//  These libraries were adapted by Bernhard Nebel for use on Arduino   //
-//    https://github.com/felias-fogg/SoftI2CMaster                      //
-//                                                                      //
-//  Special Thanks to MaxBotix Inc. for sponsoring this project!        //
-//    http://www.maxbotix.com -- High Performance Ultrasonic Sensors    //
-//                                                                      //
-//  For more information on installing the I2C libraries for Arduino    //
-//    visit http://playground.arduino.cc/Main/SoftwareI2CLibrary        //
-//////////////////////////////////////////////////////////////////////////
-
-
-//////////////////////////////////////////////////////////////////////////
-/*                   Arduino RadioHead                                  */
-//  RadioHead is a trademark of AirSpayce Pty Ltd. The RadioHead mark   //
-//  was first used on April 12 2014 for international trade, and is     //
-//  used only in relation to data communications hardware and software  //
-//  and related services. It is not to be confused with any other       //
-//  similar marks covering other goods and services.                    //
-//                                                                      //
-//  For more information                                                //
-//   http://www.airspayce.com/mikem/arduino/RadioHead/                  //
-//                                                                      //
-//  Download                                                            //
-//  http://www.airspayce.com/mikem/arduino/RadioHead/RadioHead-1.89.zip //
-//////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-//Hints on installing this code:
-// 1. You will need to install the <SoftI2CMaster.h> library before using this code.
-//      On Windows, the files are placed in C:\Program Files (x86)\Arduino\libraries\SoftI2CMaster\
-// 2. As of 10-02-14 the Arduino library page (reference above) has the wrong name for the include file
-//      it lists <SoftI2C.h> instead of <SoftI2CMaster.h> -- use the one that matches your installation.
-// 3. Make sure to load the library into the Arduino compiler. 
-//      To do this go to: SKETCH >> IMPORT LIBRARY... >> ADD LIBRARY...
-//      Then navigate to C:\Program Files (x86)\Arduino\libraries\SoftI2CMaster\SoftI2CMaster.h
-// 4. Be sure to set the SCL and SDA pins so that they match the pins you are using.
-// 5. I have included 3 working "code examples" which differ from the 3 "functions" I included.
-//      The functions are all that should be required to quickly use the I2C library to talk to a MaxSonar.
-//      The three code examples show how I would implement each of the common tasks you may wish to do.
-// 6. The included functions are as follows:
-//      A. start_sensor(addr)
-//      B. read_sensor(addr)
-//      C. change_address(oldaddr,newaddr)
-// 7. The included code examples are as follows:
-//      A. read_the_sensor_example()
-//      B. address_polling_example()
-//      C. default_address_change_example()
-// 8. You do not have to keep track of the error codes passed out by the installed functions if you do not want to.
-//      I inluded the error tracking so that it was easy for others to build a reliable system -- and to ease
-//      troubleshooting. (not using it makes for cleaner code if you trust your interface)
-
-//To get the Transiever and Reciever working...
